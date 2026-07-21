4f313ecfa8c540065414926756fc9b7a111649fc
DevloperSP <teamrvabalaji@gmail.com>
2026-07-21 18:36:20 +0530

    update by devlopersp

--- MusicSp/platforms/Youtube.py
+++ MusicSp/platforms/Youtube.py
@@ -8,9 +8,9 @@ from pyrogram.types import Message
 from py_yt import VideosSearch, Playlist
 import aiohttp
 
-API_URL = os.environ.get("MusicSp_API_URL", "None")
+API_URL = os.environ.get("MusicSp_API_URL", None)
 
-API_KEY = os.environ.get("MusicSp_API_KEY", "None") ## Get This API KEY FROM OWNER: @Spparow_92 
+API_KEY = os.environ.get("MusicSp_API_KEY", None) ## Get This API KEY FROM OWNER: @SpYtAPIBot 
 
 DOWNLOAD_DIR = "downloads"
 
